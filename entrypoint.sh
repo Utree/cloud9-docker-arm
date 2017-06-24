b2be73d428a84ea6acc8112615f7fabc9fd12fc1
Simone Nunzi <slysimo@gmail.com>
2017-06-24 22:37:42 +0000

    git clone working!

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,8 @@ if [ ! -z $COLLAB ]; then
   COLLAB="--collab"
 fi
 
+if [ ! -z $GITCLONE ]; then
+  git clone $GITCLONE /workspace
+fi
 
 /root/.c9/node/bin/node /home/cloud9ide/cloud9/server.js $AUTH $COLLAB --listen 0.0.0.0 --port 8080 -w /workspace
-
-if [ ! -z $GITCLONE ]; then
-   git clone $GITCLONE /workspace
-fi
